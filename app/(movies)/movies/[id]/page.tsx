704382eaa5f3f0b66920ff663a6d5456376e5500
chaeyn <chaulfe@gmail.com>
2025-08-30 19:20:34 +0900

    4.0 ~ 4.5 Styling, Dynamic Metadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,15 +1,28 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default function MovieDetail({
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  // 같은 API 두 번 호출 하더라도 캐시된 데이터를 불러온다
+  return {
+    title: movie.title,
+  };
+}
+
+export default function MovieDetailPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
   return (
     <div>
-      <h3>Movie Detail Page</h3>
       <Suspense fallback={<h1>Loading movie info...</h1>}>
         <MovieInfo id={id} />
       </Suspense>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,13 +2,12 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
-  const [count, setCount] = useState(0);
   const path = usePathname();
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href='/'>Home</Link>
@@ -17,9 +16,6 @@ export default function Navigation() {
         <li>
           <Link href='/about-us'>About Us</Link>
           {path === "/about-us" ? "🔥" : ""}
-        </li>
-        <li>
-          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
