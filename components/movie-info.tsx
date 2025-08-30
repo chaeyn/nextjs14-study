c435268e99be9a5852ce98277113d24628a9200a
chaeyn <chaulfe@gmail.com>
2025-08-30 20:17:59 +0900

    add Movie Detail information

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { API_URL } from "../app/constants";
 import styles from "../styles/movie-info.module.css";
 
@@ -6,8 +7,13 @@ export async function getMovie(id: string) {
   return response.json();
 }
 
+export async function getCredits(id: string) {
+  return fetch(`${API_URL}/${id}/credits`).then((res) => res.json());
+}
+
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
+  const credits = await getCredits(id);
   return (
     <div className={styles.container}>
       <img
@@ -16,12 +22,42 @@ export default async function MovieInfo({ id }: { id: string }) {
         className={styles.poster}
       />
       <div className={styles.info}>
-        <h1 className={styles.title}>{movie.title}</h1>
+        <h1 className={styles.title}>
+          <Link href={movie.homepage} target='_blank'>
+            {movie.title}
+          </Link>
+        </h1>
         <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
         <p>{movie.overview}</p>
-        <a href={movie.homepage} target='_blank'>
-          Homepage &rarr;
-        </a>
+        <div>
+          Genres: {movie.genres.map((genre) => genre.name).join(", ")}
+          <div className={styles.productionCompanies}>
+            {movie.production_companies
+              .filter((company) => company.logo_path !== null)
+              .map((company) => (
+                <div key={company.id} className={styles.companyItem}>
+                  <img
+                    className={styles.logo}
+                    src={`https://image.tmdb.org/t/p/w300${company.logo_path}`}
+                    alt={company.name}
+                  />
+                </div>
+              ))}
+          </div>
+        </div>
+        <h3>Top Credits</h3>
+        <div className={styles.credits}>
+          {credits.slice(0, 6).map((credit) => (
+            <div key={credit.id} className={styles.creditItem}>
+              <img
+                className={styles.creditLogo}
+                src={`https://image.tmdb.org/t/p/w300${credit.profile_path}`}
+                alt={credit.name}
+              />
+              <span>{credit.name}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
